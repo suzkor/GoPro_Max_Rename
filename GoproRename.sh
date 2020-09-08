70c9b0feda67f36488b1b424ccdc3db557d7a35b
Kohei Suzuki <40560877+suzkor@users.noreply.github.com>
2020-09-08 10:57:23 +0900

    Create GoproRename.sh

---
+++ GoproRename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function RenameFileName(){
+        # $1:2alphabet $2:sequence $3:file number $4:kakutyoshi
+        case "$2" in
+                "__" ) echo "${1}${3}.${4}" ;;
+                * ) echo "${1}${3}_${2}.${4}" ;;
+        esac
+}
+
+#for file in $( ls | grep -E "[A-Z]{2}[0-9_]{2}\d{4}\.\w{4}" ); do
+for file in $( ls | grep -E "^[A-Z]{2}[0-9_]{2}[0-9]{4}\.[a-zA-Z0-9]{3}$" ); do
+        newname=`RenameFileName ${file:0:2} ${file:2:2} ${file:4:4} ${file:9:4}`
+        mv $file $newname
+        echo "${file} -> $newname"
+done
